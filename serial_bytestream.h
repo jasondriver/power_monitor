a85d6a605666c9582837bdd355d739d36d539343
jasondriver <jasondriver5@hotmail.com>
2018-09-23 14:13:45 -0700

    added header, changed makefile to accomidate change

---
+++ serial_bytestream.h
@@ -0,0 +1,14 @@
+#ifndef _SERIAL_BYTESTREAM_H
+#define _SERIAL_BYTESTREAM_H
+
+#include <stdint.h>
+
+static int set_interface_attribs (int fd, int speed, int parity);
+static int set_blocking (int fd, int should_block);
+static int print_packet(int size, uint8_t packet[]);
+static int send_packet(int fd, int size, uint8_t packet[]);
+static int recieve_packet(int fd, int size, uint8_t *packet);
+static int set_com_addr(int fd, uint8_t addr[]);
+static int reset_device(int fd);
+
+#endif /* _SERIAL_BYTESTREAM_H */
