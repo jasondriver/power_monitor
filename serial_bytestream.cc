f53e662fb5654e11010b5f120c1e9063494a8cc0
jasondriver <jasondriver5@hotmail.com>
2019-01-31 12:48:24 -0800

    added device id as key in db, removed unnecissary functions

--- serial_bytestream.cc
+++ serial_bytestream.cc
@@ -79,7 +79,7 @@ class Serial {
         double receive_current();
         int receive_energy();
         void set_com_addr();
-        void reset_line();
+        void line_wait();
 
         int print_packet(int size, uint8_t packet[]);
         void print_com_addr();
@@ -390,14 +390,14 @@ void Serial::set_com_addr() {
 }
 
 /*
- *  Resets line by sending blank packet and consuming packets from device
+ *  Waitline by sending blank packet and consuming packets from device
  */
-void Serial::reset_line() {
-    int num_times_reset = 4;
-    uint8_t reset_packet[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+void Serial::line_wait() {
+    int num_times_wait = 4;
+    uint8_t blank_packet[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
     uint8_t buf[MSG_LEN];
-    for(int i = 0; i < num_times_reset; i++) {
-        send_packet(MSG_LEN, reset_packet);
+    for(int i = 0; i < num_times_wait; i++) {
+        send_packet(MSG_LEN, blank_packet);
         usleep(SEND_RECEIVE_DELAY);
         recieve_packet(MSG_LEN, buf);
     }
@@ -452,17 +452,12 @@ int Serial::get_fd() {
     return fd;
 }
 
-/*
- *
- *  Stand alone methods
- *
- */
+/* global methods */
 
 /* 
  *  Sets usb information using the open file descriptor
- *  Thanks to wallyk for the set_interface_attribs and set_blocking methods from this post on stack overflow
+ *  help with set_interface_attribs and set_blocking from
  *  https://stackoverflow.com/questions/6947413/how-to-open-read-and-write-from-serial-port-in-c
- *  added some slight modifications to them
  */ 
 static int set_interface_attribs (int fd, int speed, int parity) {
     struct termios tty;
@@ -588,139 +583,7 @@ int set_com_addr(int fd, uint8_t addr[]) {
     return 0;
 }
 
-/*
- *  Convert voltage packet to voltage value
- *  voltage = packet[1] packet[2] . packet[3]
- */
-static double convert_voltage(uint8_t packet[]) {
-    int voltage_int = packet[1] * 100 + packet[2];
-    double voltage_decimal = (double) packet[3] /100.0;
-    double ret = (double) voltage_int + voltage_decimal;
-    return ret;
-}
-
-/*
- *  Convert current packet to value
- */
-static double convert_current(uint8_t packet[]) {
-    return (double) packet[2] + ((double) packet[3] / 100.0);
-}
-
-/*
- *  Helper function for convert_power and convert_energy
- *  size = how many elements to use, offset = where in the array to start, packet = array of #s
- */
-static int hex_to_int(int size, int offset, uint8_t packet[]) {
-    uint8_t* arr =  (uint8_t*) malloc(2 * size * sizeof(uint8_t));
-    if(!arr) {
-        printf("error %d from malloc", errno);
-    return -1;
-    }
-    int j = offset;
-    for (int i = 0; i < 2 * size; i++){
-        if((i % 2) == 0) {
-            arr[i] = (packet[j] & 0xF0) >> 4;
-    } else {
-            arr[i] = packet[j] & 0x0F;
-            j++;
-    }
-    }
-
-    int base = 1;
-    int ret = 0;
-
-    for (int i = (2 * size) - 1; i > -1; i--) {
-        ret += arr[i] * base;
-    base *= 16;
-    }
-
-    free(arr);
-    return ret;
-}
-
-/*
- *  Convert average power packet to power value
- *  power = (packet[1] packet[2])base16
- */
-static int convert_power(uint8_t packet[]) {
-    return hex_to_int(2, 1, packet);
-}
-
-/*
- *  Convert energy packet to value
- */
-static int convert_energy(uint8_t packet[]) {
-    return hex_to_int(3, 1, packet);
-}
-
-/*
- *  
- *  Main send methods
- *
- */
-
-/* */
-static int receive_power(int fd) {
-    uint8_t buf[MSG_LEN];
-    memset(&buf, 0, sizeof buf);
-    send_packet(fd, MSG_LEN, READ_WATTAGE);
-    usleep(SEND_RECEIVE_DELAY);
-    recieve_packet(fd, MSG_LEN, buf);
-    return convert_power(buf);
-}
-
-static double receive_voltage(int fd) {
-    uint8_t buf[MSG_LEN];
-    memset(&buf, 0, sizeof buf);
-    send_packet(fd, MSG_LEN, READ_VOLTAGE);
-    usleep(SEND_RECEIVE_DELAY);
-    recieve_packet(fd, MSG_LEN, buf);
-    return convert_voltage(buf);
-}
-
-static double receive_current(int fd) {
-    uint8_t buf[MSG_LEN];
-    memset(&buf, 0, sizeof buf);
-    send_packet(fd, MSG_LEN, READ_CURRENT);
-    usleep(SEND_RECEIVE_DELAY);
-    recieve_packet(fd, MSG_LEN, buf);
-    return convert_current(buf);
-}
-
-static int receive_energy(int fd) {
-    uint8_t buf[MSG_LEN];
-    memset(&buf, 0, sizeof buf);
-    send_packet(fd, MSG_LEN, READ_ENERGY);
-    usleep(SEND_RECEIVE_DELAY);
-    recieve_packet(fd, MSG_LEN, buf);
-    return convert_energy(buf);
-}
-
-static void reset_line(int fd) {
-    int num_times_reset = 4;
-    uint8_t reset_packet[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    uint8_t buf[MSG_LEN];
-    for(int i = 0; i < num_times_reset; i++) {
-        send_packet(fd, MSG_LEN, reset_packet);
-        usleep(SEND_RECEIVE_DELAY);
-        recieve_packet(fd, MSG_LEN, buf);
-    }
-    return;
-}
-
-/* 
- *  TODO: resets usb device
- */
-static int reset_device(int fd) {
-    // doesn't work
-    int rc = ioctl(fd, USBDEVFS_RESET, 0);
-    if (rc < 0) {
-        perror("Error in ioctl");
-        return -1;
-    }
-    return 0;
-}
-
+/* return date and time as string, works on linux */
 string current_time_and_date() {
     time_t raw_time = time(nullptr);
     stringstream ss;
@@ -728,15 +591,25 @@ string current_time_and_date() {
     return ss.str();
 }
 
-void return_formated_sql_insert_string(string out, string date_time, int device_id, double voltage, double current, int power, int energy) {
+/* 1st argument is output string, need to use string c_str() to convert to c style char* for sqlite3 */
+void return_formated_sql_insert_string(string* out, string date_time, int device_id, double voltage, double current, int power, int energy) {
     stringstream ss;
     ss << "INSERT INTO DAILY_POWER (DATE,DEVICE_ID,VOLTAGE,CURRENT,POWER,ENERGY) ";
     ss << "VALUES ('" << date_time << "', " << device_id << ", " << setw(5) << voltage;
     ss << ", " << setw(4) << current << ", " << power << ", " << energy << " );";
-    out = ss.str();
+    
+    *out = ss.str();
+
     return;
 }
 
+/* formatted print to cout for sql info */
+void sql_cout_msg(string the_time, int id, double voltage, double current, int power, int energy) {
+    cout << the_time << ", " << id << ", " << voltage << ", " << current << ", " << power << ", " << energy << endl;
+    return;
+}
+
+/* sqlite3 exec callback function */
 static int callback(void *not_used, int argc, char **argv, char **az_col_name) {
    int i;
    for(i = 0; i<argc; i++) {
@@ -748,19 +621,14 @@ static int callback(void *not_used, int argc, char **argv, char **az_col_name) {
 
 int main() 
 {
-    printf("starting...\n");
-    fflush(stdout);
-   
     /* get time */
     string the_time = current_time_and_date();
-    cout << "Current time: " << current_time_and_date() << "\n";
+    cerr << "starting at current time: " << current_time_and_date() << "\n";
 
     // open usb device
     char portname[] = "/dev/ttyUSB0";
     char portname1[] = "/dev/ttyUSB1";
-//    float p1,p2, v1,v2, c1, c2, e1,e2;
 
-/*
     int fd  = open (portname,  O_RDWR | O_NOCTTY | O_SYNC);
     int fd1 = open (portname1, O_RDWR | O_NOCTTY | O_SYNC);
 
@@ -776,7 +644,6 @@ int main()
     // set no blocking 
     set_blocking (fd , 0);
     set_blocking (fd1, 0);
-*/
 
     // set communication address as 192.168.1.2 (not for LAN, but for usb to device)
     uint8_t com_addr[] = {192, 168, 1, 1};
@@ -793,34 +660,8 @@ int main()
     s.set_com_addr();
     s1.set_com_addr();
     // print all
-    s.print_all();
-    s1.print_all();
-
-    /*
-    for (int i = 0; i < 2; i++) {
-        s.reset_line();
-        s1.reset_line();
-        printf("Wattage is: %d ,  %d\n", s.receive_power(), s1.receive_power() );
-    }
-
-    for (int i = 0; i < 2; i++) {
-        s.reset_line();
-        s1.reset_line();
-        printf("Voltage is: %f ,  %f\n", s.receive_voltage(), s1.receive_voltage()  );
-    }
-
-    for (int i = 0; i < 2; i++) {
-        s.reset_line();
-        s1.reset_line();
-        printf("Current is: %f ,  %f\n", s.receive_current(), s1.receive_current());
-    }
-
-    for (int i = 0; i < 2; i++) {
-        s.reset_line();
-        s1.reset_line();
-        printf("Energy is: %d ,  %d\n", s.receive_energy() , s1.receive_energy() );
-    }
-    */
+    //s.print_all();
+    //s1.print_all();
 
     /* sqlite3 database variables */
     sqlite3 *db;
@@ -838,100 +679,64 @@ int main()
     }
 
     /* Get info */
-    int device_id = 1;
     double voltage = 0.0,   v2=0;
     double  current =  0.0, c2=0;
     int power = 0,  p2=0;
     int energy = 0, e2=0;
-    FILE *ostream;
 
-//  ostream = fopen("data.csv","w");
-//  fprintf(ostream," time, p1,p2");
-
-    /* set timer */
-    /*
-    struct sigaction sa;
-    struct itimerval timer;
-
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = &periodic_function;
-    sigaction(SIGVALRM, &sa, NULL);
-
-    timer.it_value.tv_sec = 0.5;
-    timer.it_interval.tv_sec = 0.5;
-
-    setitimer(ITIMER_VIRTUAL, &timer, NULL);
-    */
-
-    for (int k = 0; k < 99999999; k++) {
+    /* continuous loop */
+    for (uint k = 0;; k++) {
 
         the_time = current_time_and_date();
 
         /* Query device */
-        s.reset_line();
+        s.line_wait();
         voltage = s.receive_voltage();
-        s.reset_line();
+        s.line_wait();
         current =  s.receive_current();
-        s.reset_line();
+        s.line_wait();
         power = s.receive_power();
-        s.reset_line();
+        s.line_wait();
         energy =  s.receive_energy();
 
-        s1.reset_line();
+        s1.line_wait();
         v2 = s1.receive_voltage();
-        s1.reset_line();
+        s1.line_wait();
         c2 =  s1.receive_current();
-        s1.reset_line();
+        s1.line_wait();
         p2 = s1.receive_power();
-        s1.reset_line();
+        s1.line_wait();
         e2 =  s1.receive_energy();
 
-//      printf( "%d,%s,%f8.2,%f8.2 \n",k,current_time_and_date,power,p2); 
-        printf( "%d,%8d,%8d \n",k,power,p2); 
-        fflush(stdout);
-
         /* Create SQL statement */
-        string sql;
+        string* sql = new string[180];
 	return_formated_sql_insert_string(sql, the_time, 0, voltage, current, power, energy);
-	string sql2;
+	string* sql2 = new string[180];
         return_formated_sql_insert_string(sql2, the_time, 1, v2, c2, p2, e2);
 
         /* Execute SQL statement */
-        rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
+        rc = sqlite3_exec(db, sql->c_str(), callback, 0, &zErrMsg);
 
         //Catch the error
         if( rc != SQLITE_OK ){
             fprintf(stderr, "SQL error: %s\n", zErrMsg);
             sqlite3_free(zErrMsg);
         } else {
-            cout << "Records created successfully\n" << sql << "\n";
+            sql_cout_msg(the_time, 0, voltage, current, power, energy);
         }
-        
-        rc2 = sqlite3_exec(db, sql2.c_str(), callback, 0, &zErrMsg);
+       
+        rc2 = sqlite3_exec(db, sql2->c_str(), callback, 0, &zErrMsg);
         // Catch the error
         if( rc2 != SQLITE_OK ){
             fprintf(stderr, "SQL error: %s\n", zErrMsg);
             sqlite3_free(zErrMsg);
         } else {
-            cout << "Records created successfully\n" << sql << "\n";
-        }
+            sql_cout_msg(the_time, 1, v2, c2, p2, e2);
+	}
        
 
     }
 
-    /* Create SQL statement */
-    //char const* sql = return_formated_sql_insert_string(current_time_and_date(), device_id, voltage, current, power, energy).c_str();
-
-    /* Execute SQL statement */
-    //rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-    /*   
-    if( rc != SQLITE_OK ){
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
-    } else {
-        cout << "Records created successfully\n" << sql << "\n";
-    }
-    */
     sqlite3_close(db);
 
     printf("ending...\n");
@@ -939,6 +744,5 @@ int main()
  
     s.close_port();
  
-    //reset_device(fd);
     return 0;
 }
--- serial_bytestream.h
+++ serial_bytestream.h
@@ -1,12 +1,3 @@
 #ifndef _SERIAL_BYTESTREAM_H
 #define _SERIAL_BYTESTREAM_H
-
-#include <stdint.h>
-
-static int send_packet(int fd, int size, uint8_t packet[]);
-static int recieve_packet(int fd, int size, uint8_t *packet);
-static int set_com_addr(int fd, uint8_t addr[]);
-static double convert_voltage(uint8_t packet[]);
-static int reset_device(int fd);
-
 #endif /* _SERIAL_BYTESTREAM_H */
